dba41fe6cc4ecdcccd7e4334e322dfb5fbf81455
Rayer <rayershih@gmail.com>
2014-05-05 08:16:46 +0800

    Draft init

--- src/smngt/main.cpp
+++ src/smngt/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+class EntrySocketTranslator;
+class ExitSocketTranslator;
+
+class ISocketTranslator {
+public:
+    EntrySocketTranslator* getEntrySocketTranslator;
+    ExitSocketTranslator* getExitSocketTranslator;
+};
+
+class ISocketManager {
+public:
+    //These defines Events
+    void onStartGetSocketData();
+    void onFinishGetSocketData();
+    void onStartSendSocketData();
+    void onFinishSendSocketData();
+};
+
+
 int main(int argc, char* argv[]) {
     std::cout << "Hello World!" << std::endl;
 }
